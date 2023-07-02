f49c2abbe31c64b5841fcbd7bb81d848f0bffa21
goldin2008 <goldin20082011@gmail.com>
2023-07-02 07:16:49 -0400

    Update

---
+++ src/llm/test.py
@@ -0,0 +1,18 @@
+def pythagorus(num1, num2=0):
+    """
+    ### Description:
+
+    Calculates the root-sum-of-square of two numbers
+
+    ### Args:
+
+    `num1`: The first number
+
+    `num2`: The second number, default `0`
+
+    ### Returns:
+
+    The result of the **pythagorus** formula
+
+    $$dist = \\sqrt { a^2+b^2 }$$
+    """
