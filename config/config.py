8004035db1a4d0e87e01c64667323951195b5e31
goldin2008 <goldin20082011@gmail.com>
2023-07-04 07:57:14 -0400

    Update ingestion

--- config/config.py
+++ config/config.py
@@ -1,23 +1,26 @@
-import os
 import json
+import os
+
 import yaml
 
-##################Load config.json and get environment variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open("config/config.json", "r") as f:
+    """
+    Load config.json and get environment variables
+    """
+    config = json.load(f)
 
-with open("config.yaml", 'r') as f:
+with open("config/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-# output_folder_path = os.path.join(config['output_folder_path']) 
-# test_data_path = os.path.join(config['test_data_path']) 
+# output_folder_path = os.path.join(config['output_folder_path'])
+# test_data_path = os.path.join(config['test_data_path'])
 # prod_deployment_path = os.path.join(config['prod_deployment_path'])
 
 # INPUT_FOLDER_PATH = os.path.join(os.path.abspath('../'),
 #                                  'data',
 #                                  config['input_folder_path'])
 # DATA_PATH = os.path.join(os.path.abspath('../'),
-#                          'data',
+#                          'data',git
 #                          config['output_folder_path'])
 # TEST_DATA_PATH = os.path.join(os.path.abspath('../'),
 #                               'data',
@@ -29,13 +32,14 @@ with open("config.yaml", 'r') as f:
 #                                     'model',
 #                                     config['prod_deployment_path'])
 
-INPUT_FOLDER_PATH = os.path.join(os.path.abspath('./'),
-                                 config['input_folder_path'])
-DATA_PATH = os.path.join(os.path.abspath('./'),
-                         config['output_folder_path'])
-TEST_DATA_PATH = os.path.join(os.path.abspath('./'),
-                              config['test_data_path'])
-OUTPUT_MODEL_PATH = os.path.join(os.path.abspath('./'),
-                          config['output_model_path'])
-PROD_DEPLOYMENT_PATH = os.path.join(os.path.abspath('./'),
-                                    config['prod_deployment_path'])
+INPUT_FOLDER_PATH = os.path.join(
+    os.path.abspath("./"), config["input_folder_path"]
+)
+DATA_PATH = os.path.join(os.path.abspath("./"), config["output_folder_path"])
+TEST_DATA_PATH = os.path.join(os.path.abspath("./"), config["test_data_path"])
+OUTPUT_MODEL_PATH = os.path.join(
+    os.path.abspath("./"), config["output_model_path"]
+)
+PROD_DEPLOYMENT_PATH = os.path.join(
+    os.path.abspath("./"), config["prod_deployment_path"]
+)
--- src/llm/ingestion.py
+++ src/llm/ingestion.py
@@ -1,44 +1,63 @@
-import os
+import glob
 import json
-import pandas as pd
-import numpy as np
+import logging
+import os
+import sys
 from datetime import datetime
 
-import logging
-import glob
-from config import DATA_PATH, OUTPUT_MODEL_PATH, TEST_DATA_PATH
+import numpy as np
+import pandas as pd
 
+# Now you can import the module from the subfolder
+from config.config import DATA_PATH, OUTPUT_MODEL_PATH, TEST_DATA_PATH
+
+# Add the parent_folder directory to the Python import path
+parent_folder_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../..")
+)
+# print(os.path.dirname(__file__))
+# print(parent_folder_path)
+sys.path.append(parent_folder_path)
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
-#############Load config.json and get input and output paths
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open("config/config.json", "r") as f:
+    """
+    Load config.json and get input and output paths
+    """
+    config = json.load(f)
 
-input_folder_path = config['input_folder_path']
-output_folder_path = config['output_folder_path']
+input_folder_path = config["input_folder_path"]
+output_folder_path = config["output_folder_path"]
 
 
-#############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    """
+    Function for data ingestion
+    :return:
+    """
+    # check for datasets, compile them together, and write to an output file
     df = pd.DataFrame()
 
     # recursivly search direcotries and read .csv files.
-    datasets = glob.glob(f'{input_folder_path}/*.csv', recursive=True)
+    datasets = glob.glob(f"{input_folder_path}/*.csv", recursive=True)
     df = pd.concat(map(pd.read_csv, datasets))
 
     df_final = df.drop_duplicates()
-    df_final.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
+    df_final.to_csv(
+        os.path.join(output_folder_path, "finaldata.csv"), index=False
+    )
 
     file_list = [os.path.basename(filepath) for filepath in datasets]
 
-    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), "w") as f:
-        f.write(f"Ingestion date: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+    with open(os.path.join(output_folder_path, "ingestedfiles.txt"), "w") as f:
+        f.write(
+            f"Ingestion date: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n"
+        )
         for file in file_list:
             f.write(file + "\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     merge_multiple_dataframe()
